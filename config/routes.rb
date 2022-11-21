fe849e1d8f46d92ead5f57d1e9ce9547f0a74913
robin rettien <robinrettien@gmail.com>
2022-11-21 17:17:20 +0100

    adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  resources :athletes, only: [:index, :new, :create, :show, :edit, :update] do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, only: [] do
+    resources :reviews, only: [:new, :create]
+  end
+  resources :bookings, only: [:edit, :update]
+  get "/dashboard", to: "pages#dashboard"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
