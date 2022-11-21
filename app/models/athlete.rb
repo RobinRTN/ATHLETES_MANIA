dfa4d798ab0ad5395c117fd38c03476f89fcf830
Shayneze Menegady <shayneze.menegady@gmail.com>
2022-11-21 17:36:55 +0100

    ajouter validations modeles

--- app/models/athlete.rb
+++ app/models/athlete.rb
@@ -1,4 +1,13 @@
 class Athlete < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
+  validates :first_name, presence: true, uniqueness: { scope: :last_name }
+  validates :last_name, presence: true
+  validates :price, presence: true
+  validates :age, presence: true, numericality: { greater_than_or_equal_to: 18, only_integer: true }
+  validates :location, presence: true
+  validates :summary, presence: true
+  validates :features, presence: true
+  validates :sport, presence: true
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,12 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :athlete
   has_many :reviews
+
+  validates :user_id, uniqueness: { scope: :athlete_id }
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :end_date, comparison: { greater_than: :start_date }
+  validates :status, presence: true
+  validates :price, presence: true
+  validates :duration, presence: true
 end
