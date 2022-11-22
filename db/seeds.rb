7d876a60934171654b33393d3e213bd4560e991b
Shayneze Menegady <shayneze.menegady@gmail.com>
2022-11-22 12:06:23 +0100

    truc

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning athletes database..."
+Athlete.destroy_all
+puts "Creating athletes..."
+Athlete.create!(first_name: "Karim", last_name: "Benzema", price_per_day: 1000, age: 35, location: "Algerie", summary: "xxx" , features: "DZ", sport: "Football", user_id: 1 )
+Athlete.create!(first_name: "Lebron", last_name: "James", price_per_day: 850, age: 37, location: "States", summary: "xxx" , features: "aaa", sport: "Basketball", user_id: 1 )
+puts "Finished!"
