efaa948f71e91fe7e0ef60c719af1bd8f9c28090
Kevin Blakime <blakime.kevin@gmail.com>
2022-11-22 12:14:00 +0100

    Dashboard part: controller + view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :athletes
+  has_many :bookings
   has_one_attached :photo
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
