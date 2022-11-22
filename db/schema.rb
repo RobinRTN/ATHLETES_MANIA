c504455994529d57c83914180d0a3a1f1b79820a
Shayneze Menegady <shayneze.menegady@gmail.com>
2022-11-22 17:45:17 +0100

    add columns availability and gender to athletes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_22_152230) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_22_161432) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,7 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_152230) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "availability", default: false
+    t.boolean "availability", default: true
     t.string "gender"
     t.index ["user_id"], name: "index_athletes_on_user_id"
   end
