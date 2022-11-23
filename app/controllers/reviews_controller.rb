d488a8635d4965acae5d41c20d22dfc40bd51d25
Gabriel de Monts <gabriel.demonts@gmail.com>
2022-11-23 14:04:26 +0100

    creating reviews view

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,14 @@
 class ReviewsController < ApplicationController
 
   def new
+    @booking = Booking.find(params[:booking_id])
+    @athlete = @booking.athlete_id
     @review = Review.new
+    @review.booking = @booking
     authorize @review
+
+    @reviews = policy_scope(Review)
+    @athlete.reviews
   end
 
   # add current_user w/ pundit
