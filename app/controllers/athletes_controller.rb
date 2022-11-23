2abda2c36e3cdea44b36c790abbe3a6c7093f186
robin rettien <robinrettien@gmail.com>
2022-11-23 13:05:15 +0100

    added index and show for user authorization

--- app/controllers/athletes_controller.rb
+++ app/controllers/athletes_controller.rb
@@ -1,4 +1,5 @@
 class AthletesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_athlete, only: [:show, :edit, :update]
 
   def index
