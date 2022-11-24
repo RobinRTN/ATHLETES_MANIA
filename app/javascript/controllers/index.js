7ac9f425202f87627480edb3e2785c598bf9e4c0
Gabriel de Monts <gabriel.demonts@gmail.com>
2022-11-24 14:16:20 +0100

    search bar functional

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -15,3 +15,6 @@ application.register("flatpickr", FlatpickrController)
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import MapController from "./map_controller"
+application.register("map", MapController)
